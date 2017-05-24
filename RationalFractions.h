7a4c86bd4a843c9f18deacbbf9c1fbd048c33f22
kvarzarev <kvarzarev@gmail.com>
2017-05-24 17:29:43 +0700

    24-05-2017

--- RationalFractions.h
+++ RationalFractions.h
@@ -12,12 +12,12 @@ class RationalFractions
 	void reduction(); //сокращение дроби
 public:
 	RationalFractions(); //конструктор по умолчанию
-	RationalFractions(char*, int, int); //конструктор с параметрами
-	RationalFractions(int num, int den);
-	RationalFractions(const RationalFractions& F);//конструктор копирования
+	RationalFractions(char*, int, int); //конструктор с параметрами-1
+	RationalFractions(int, int); //конструктор с параметрам-2
+	RationalFractions(const RationalFractions&F);//конструктор копирования
 	~RationalFractions(); //деструктор
 	static int GetCount(); // счетчик количества созданных объектов
-	//перегрузка операторов
+						   //Перегрузка операторов
 	RationalFractions operator+(RationalFractions &); // перегрузка оператора сложения
 	RationalFractions operator-(RationalFractions &);// перегрузка оператора вычитания
 	RationalFractions operator*(RationalFractions &);// перегрузка оператора умножения
@@ -29,11 +29,11 @@ public:
 	RationalFractions &operator = (RationalFractions &F); //перегрузка оператора присваивания
 	friend ostream& operator <<(ostream&out, RationalFractions&F); //перегрузка оператора cout
 	friend istream& operator >> (istream&in, RationalFractions&F);//перегрузка оператора cin
-	
-	void getFraction(); //метод вывода дроби (без имени)
+
+	void getFraction(); //функция вывода дроби (без имени)
 	void getResult(); //вывод результатов вычислений в виде правильной простой дроби
-	void setNameFraction(char *);
-	void setNumerator(int); 
+	void setNameFraction(char*);
+	void setNumerator(int);
 	void setDenominator(int);
 	char *getNameFraction();
 	int getNumerator();
